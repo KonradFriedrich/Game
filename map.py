435aa294cadbc4ed5198defa50ef030d88cf501e
PrankYou <GamePrankYou@users.noreply.github.com>
2020-09-03 18:49:32 +0200

    small changes

--- map.py
+++ map.py
@@ -18,21 +18,28 @@ SeeWorld = []
 
 
 # map drawing function
-def MapDraw(bgx, bgy, World):
+def MapDraw(bgx, bgy, WorldDict):
 
 
     # player fov box rounded too hundreds, +100 bcs rounded up
     PlayerFovX = range(int(math.ceil((bgx - screenX - 100) / 100.0)) * 100, int(math.ceil(bgx / 100.0)) * 100, 100)
     PlayerFovY = range(int(math.ceil((bgy - screenX - 100) / 100.0)) * 100, int(math.ceil(bgy / 100.0)) * 100, 100)
+    print(PlayerFovX)
+    print(PlayerFovY)
 
 
 
     # loop through tiles
-    for i in World:
-        # loop through corners (not the first part bcs pic!)
-        for j in islice(World, 1, None):
-            if World[i][j][1] in PlayerFovY and World[i][j][2] in PlayerFovX:
-                screen.blit(World[i[1]], (World[i][1][1], World[i][1][2]))
+    for i in WorldDict:
+        #print(WorldDict[i])
+        #print(WorldDict[i]["corners"])
+        for j in WorldDict[i]["corners"]:
+            #print(j)
+            if j[0] in PlayerFovY and j[1] in PlayerFovX:
+                #screen.blit(WorldDict[i["loc"]], (WorldDict[i]["corners"]["topl"][0], WorldDict[i]["corners"]["topl"][1]))
+                print(f"{j} + see")
+                print(WorldDict[i]["loc"])
+                screen.blit(WorldDict[i]["loc"], (0, 0))
 
                 # Maybe faster like this:
                 # if World[i[j[1]]] in range(int(math.ceil((bgx - screenX - 100) / 100.0)) * 100, int(math.ceil(bgx / 100.0)) * 100, 100) and World[i[j[2]] in range(int(math.ceil((bgy - screenX - 100) / 100.0)) * 100, int(math.ceil(bgy / 100.0)) * 100, 100):
@@ -50,13 +57,10 @@ def MapSlicer(Map, screenW, screenH):
 
     # getting the sizes
     im = Image.open(Map)
-    print(im.width)
+    #print(im.width)
     imgwidth, imgheight = im.size
-    #im = pygame.image.load(Map)
-    #imgwidth = im.get_width
-    #imgheight = im.get_height
-    print(imgwidth)
-    print(imgheight)
+    #print(imgwidth)
+    #print(imgheight)
 
 
     rows = imgwidth / screenW / 4
@@ -68,10 +72,8 @@ def MapSlicer(Map, screenW, screenH):
     for i in range(0, math.ceil(rows)):
         for j in range(0, math.ceil(columns)):
             tile = im.crop((i * twidth, j * theight, (i + 1) * twidth, (j + 1) * theight))
-            #img = im.crop(tile)
             x += 1
             # save the image
-            print(x)
             tile.save(f"img/tile-{x}-{i + 1}#{j + 1}-.png")
             # tile corners for loading points (and round up to hundreds for easier calc)
             topl = [int(math.ceil(theight * i / 100.0)) * 100, int(math.ceil(twidth * j / 100.0)) * 100]
@@ -79,7 +81,7 @@ def MapSlicer(Map, screenW, screenH):
             botl = [int(math.ceil(theight * (i + 1) / 100.0)) * 100, int(math.ceil(twidth * j / 100.0)) * 100]
             botr = [int(math.ceil(theight * (i + 1) / 100.0)) * 100, int(math.ceil(twidth * (j + 1) / 100.0)) * 100]
 
-            # create world list
+            # create world dict
             World["Tile" + str(x)] = {}
             World["Tile" + str(x)]["loc"] = f"img/tile-{x}-{i + 1}#{j + 1}-.png"
                 # ("img/tile-" + x + "-" + str(i) + "#" + str(j) + "-" + ".png")
@@ -89,3 +91,4 @@ def MapSlicer(Map, screenW, screenH):
 
 
 MapSlicer('img/floorwood.png', 50, 50)
+MapDraw(300, 300, World)
