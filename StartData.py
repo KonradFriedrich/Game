fbc43f058f0d4342dd63f6bf7d40b67f18c4c7e8
PrankYou <GamePrankYou@users.noreply.github.com>
2022-03-10 19:00:10 +0100

    Update For Bell

--- StartData.py
+++ StartData.py
@@ -56,6 +56,7 @@ class Playerob(pg.sprite.Sprite):
         self.playerEffect = []
         self.invincible = pg.time.Clock()
         self.rect = pg.rect
+        self.nodes = []
 
 
     def animationspliter(self, image, frames = 1):
@@ -87,15 +88,21 @@ class Playerob(pg.sprite.Sprite):
         # obstacle blocking
 
         # "theoretically" move the player then look if he intersects with an obstacle and then either leave it be or move him back
-        self.rect.move(x, 0)
-        if pg.sprite.collide_rect(player, enemy):
-            x = 0
-        self.rect.move(-x, 0)
 
-        self.rect.move(0, y)
-        if pg.sprite.collide_rect(player, enemy):
+        #print("1:  " + str(self.rect.left))
+        #print(x)
+        self.rect.move_ip(x, 0)
+        #print("2:  " + str(self.rect.left))
+        if pg.sprite.collide_rect(player, obstacle1):
+            x = 0
+        #print("3:  " + str(self.rect.left))
+        self.rect.move_ip(-x, 0)
+        #print("4:  " + str(obstacle1.rect))
+
+        self.rect.move_ip(0, y)
+        if pg.sprite.collide_rect(player, obstacle1):
             y = 0
-        self.rect.move(0, -y)
+        self.rect.move_ip(0, -y)
 
 
 
@@ -103,6 +110,7 @@ class Playerob(pg.sprite.Sprite):
 
         #dont fuckin g move the rect move the map!!!!!!!!!!!!!!!!!!!!!!!!! change this later!!!!!!!!!!
         #change bg x/y not player rect!!!!
+        print(x)
         bgx = x + bgx
         bgy = y + bgy
         self.rect.move(x, y)
@@ -222,6 +230,124 @@ class Enemyob(pg.sprite.Sprite):
             screen.blit(self.img[12 * 5 + frame], (x, y))
 
 
+    def pathfinding(self, target):
+        path1 = path(self, target)
+        subpaths = path1.intersect()
+        if len(subpaths) == 2:
+            self.node.append(subpaths[0])
+            self.node.append(subpaths[1])
+        elif len(subpaths) == 1:
+            self.node.append(subpaths[0])
+        else:
+            print("paths")
+
+
+
+
+
+
+
+
+
+
+class path(pg.sprite.Sprite):
+    def __init__(self, start, target):
+        super().__init__()
+        self.startx = start.rect.center[0]
+        self.starty = start.rect.center[1]
+        self.targetx = target.rect.center[0] + 10
+        self.targety = target.rect.center[1] + 10
+
+        self.rect = [self.startx, self.starty, self.targetx, self.targety]
+
+        self.length = abs(self.startx - self.targetx + self.starty - self.targety)
+
+        #basically the line just wider so it looks cleaner when the enemy passes the obs probably not gonna use it for now
+        #self.wideline =
+
+    def intersect(self):
+        paths = []
+        print(1)
+        intersects = pg.sprite.spritecollide(player, enemies_group, False)
+        print(intersects)
+        print(2)
+        #not over 8 nodes bcs 9+ is 516 possible paths -> too much computing
+        if 8 < len(intersects) > 0:
+            #find out closest to start + sorting algorythm may be uselfull?
+            distance1 = 1000000
+            for i in intersects:
+                distance2 = abs(self.startx - i.rect.center[0] + self.starty - i.rect.center[1])
+                if distance2 > distance1:
+                    distance1 = distance2
+                    close = i
+
+            #possible corners for first obj
+            #way to corner + extra then check if intersect because without extra definitly intersect
+            path1 = ""
+            path2 = ""
+            tl = path(self.start, close.rect.topleft)
+            tl[0] += -3
+            tl[1] += 3
+            bl = path(self.start, close.rect.topleft)
+            bl[0] += -3
+            bl[1] += -3
+            tr = path(self.start, close.rect.topleft)
+            tr[0] += 3
+            tr[1] += 3
+            br = path(self.start, close.rect.topleft)
+            br[0] += 3
+            br[1] += -3
+            print("1")
+            #check for intersects in same ob, if yes delete path, then for intersects in other obj and find ways around
+            cornerpaths = [tl, bl, tr, br]
+
+
+            for j in cornerpaths:
+                if len(pg.sprite.spritecollide(j, enemies_group, False)) == 0:
+                    print("2")
+                    if path1 == "":
+                        path1 = j
+                    else:
+                        path2 = j
+
+                paths = [path1, path2]
+
+
+
+
+
+
+        elif len(intersects) > 8:
+            print("too many obstacles")
+
+        else:
+            print("nothing")
+
+        return paths
+
+
+
+
+class obstacle(pg.sprite.Sprite):
+    def __init__(self, topleft, bottomright, image):
+        #create coords by usinjg topleft and bottomright corner
+        super().__init__()
+        self.rect = ((topleft[0], topleft[1]), (bottomright[0] - topleft[0], bottomright[1] - topleft[1]))
+        self.tl = topleft
+        self.br = bottomright
+        self.img = pg.image.load(image)
+
+        #self.rect = (self.tl[0], self.tl[1], self.br[0], self.br[1])
+
+    def simpledraw(self):
+        self.rect = ((self.tl[0] + bgx, self.tl[1] + bgy), (self.br[0] - self.tl[0], self.br[1] - self.tl[1]))
+        screen.blit(self.img, (self.tl[0] + bgx, self.tl[1] + bgy))
+        #pg.draw.rect(screen, (0, 0, 0), (bgx self.rect)
+
+
+
+
+
 
 
 
@@ -239,5 +365,8 @@ player_group.add(player)
 enemies_group = pg.sprite.Group()
 enemies_group.add(enemy)
 
+
 obstacles = pg.sprite.Group()
 obstacles.add(enemy)
+obstacle1 = obstacle([400, 400], [450, 450], "img/red50.jpeg")
+obstacles.add(obstacle1)
